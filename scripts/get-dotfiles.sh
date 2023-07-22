556418f4e985ac43283a02e6cb11013e792aafc8
Julien Kassar <code@juli3n.xyz>
2023-07-22 14:36:41 +0200

    Remove vendor directory

--- scripts/get-dotfiles.sh
+++ scripts/get-dotfiles.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 DOTFILES_PATH="${HOME}/bin/dotfiles"
-DOTFILES_URL="https://github.com/juliengk/dotfiles"
+DOTFILES_URL="https://github.com/juli3nk/dotfiles"
 
 get_latest_tag() {
 	git ls-remote --tags ${DOTFILES_URL}.git | grep -v "{}" | awk -F'/' '{ print $3 }' | sort -V | tail -n 1
